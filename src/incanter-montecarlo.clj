702e70a5398bd85baecff2b1f3c5a2e5ad04ddd8
scientific-coder <public-code@bernard-hugueney.org>
2011-07-22 03:40:10 +0200

    first Clojure implementation using Incanter

---
+++ src/incanter-montecarlo.clj
@@ -0,0 +1,48 @@
+# for now used in incanter console only 6x slower than C++ single core and 18x than multicore !
+(use '(incanter core stats))
+
+(comment 
+  (cern.jet.random.tdouble.Normal/staticNextDouble mean sd)
+  (new cern.jet.random.tdouble.Normal 10 5 (new cern.jet.random.tdouble.engine.DoubleMersenneTwister )))
+#http://incanter.org/docs/parallelcolt/api/cern/jet/random/tdouble/Normal.html
+
+# :TODO: implement formula with cdf-normal
+
+(defn montecarlo-put-call [strike-price stock-price]
+  (let [n-steps 365
+        num-trials 200000
+        years-to-maturity 1.
+        risk-free-rate 0.03
+        volatility 0.2]
+    (letfn [(f
+             [value]
+             (/ (* value (java.lang.Math/exp (* (- risk-free-rate)
+                                                years-to-maturity)))
+                num-trials))
+            (compute-step
+             [rnd-nums-init]
+             (loop [rnd-nums rnd-nums-init
+                    i n-steps
+                    result stock-price]
+               (if (== i 0)
+                 [result rnd-nums]
+                 (recur (next rnd-nums)
+                        (dec i)
+                        (* result (+ 1. (* (first rnd-nums)
+                                           (java.lang.Math/sqrt (/ years-to-maturity
+                                                                   n-steps)))
+                                     (* risk-free-rate
+                                        (/ years-to-maturity n-steps))))))))]
+      (loop [trial num-trials
+             put-value 0.
+             call-value 0.
+             random-nbs (sample-normal (* n-steps num-trials) :mean 0. :sd volatility)]
+        (if (== trial 0)
+          (map f [put-value call-value])
+          (let [[computed-price next-random-nbs] (compute-step random-nbs)
+                delta-price (- strike-price computed-price)
+                [next-put next-call] (if (pos? delta-price)
+                                       [(+ put-value delta-price) call-value]
+                                       [put-value (- call-value delta-price)])]
+            ( recur (dec trial) next-put next-call next-random-nbs)))))))
+(time (montecarlo-put-call 110 100))
