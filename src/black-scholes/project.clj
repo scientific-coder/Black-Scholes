3c8cbab0e2f4c0136d4e94cd6198808c84ce3419
scientific-coder <public-code@bernard-hugueney.org>
2011-07-23 12:10:41 +0200

    moved to Clojure 1.3 away from incanter with project.clj in a dummy lein project

---
+++ src/black-scholes/project.clj
@@ -0,0 +1,4 @@
+(defproject black-scholes "1.0.0-SNAPSHOT"
+  :description "Put Call pricing with montecarlo simulation and Black-Scholes forumla"
+  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
+                 [incanter/parallelcolt "0.9.4"]])
