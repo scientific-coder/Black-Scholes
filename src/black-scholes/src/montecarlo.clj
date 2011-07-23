5af536ef49733e37900321a1fd332a44dc8f652d
scientific-coder <public-code@bernard-hugueney.org>
2011-07-23 13:28:51 +0200

    some type hinting attempt

--- src/black-scholes/src/montecarlo.clj
+++ src/black-scholes/src/montecarlo.clj
@@ -11,9 +11,11 @@
   (:import  cern.jet.random.tdouble.Normal)
   (:import  cern.jet.random.tdouble.engine.DoubleMersenneTwister)
   )
-(defn montecarlo-put-call [strike-price stock-price]
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true) 
+(defn montecarlo-put-call [^double strike-price ^double stock-price]
   (let [n-steps 365
-        n-trials 200
+        n-trials 200000
         years-to-maturity 1.
         risk-free-rate 0.03
         volatility 0.2
@@ -26,7 +28,8 @@
                                     (* volatility (Math/sqrt years-to-maturity)))
                               d2 (- d1 (* volatility (Math/sqrt years-to-maturity)))
                               call-price (- (* stock-price (cdf-normal d1))
-                                            (* strike-price (Math/exp (* (- risk-free-rate) years-to-maturity))
+                                            (* strike-price
+                                               (Math/exp (* (- risk-free-rate) years-to-maturity))
                                                (cdf-normal d2)))
                               put-price (- (* strike-price
                                               (Math/exp (* (- risk-free-rate) years-to-maturity))
@@ -37,10 +40,10 @@
     (letfn [(f
              [value]
              (/ (* value (Math/exp (* (- risk-free-rate)
-                                                years-to-maturity)))
+                                      years-to-maturity)))
                 n-trials))
             (compute-step
-             [price rnd-number]
+             ^:static ^double [^double price ^double rnd-number]
              (* price (+ 1.
                          (* rnd-number (Math/sqrt (/ years-to-maturity n-steps)))
                          (* risk-free-rate (/ years-to-maturity n-steps)))))
@@ -52,17 +55,20 @@
                (fn []
                  (future
                    (let [double-normal-rng (Normal. 0. volatility
-                                                (DoubleMersenneTwister. (swap! seed inc)))]
-                     (loop [put-value 0.
-                            call-value 0.]
+                                                    (DoubleMersenneTwister. (swap! seed inc)))]
+                     (loop [put-value  0.
+                            call-value  0.]
                        (if (neg? (swap! n-iters-todo dec))
                          [put-value call-value]
-                         (let [delta-price (- strike-price (reduce compute-step stock-price
-                                                                   (repeatedly n-steps #(.nextDouble double-normal-rng 0. volatility))))
+                         (let [delta-price (- strike-price
+                                              (reduce compute-step stock-price
+                                                      (repeatedly n-steps
+                                                                  #(.nextDouble double-normal-rng
+                                                                                0. volatility))))
                                [next-put-value next-call-value] (if (pos? delta-price)
                                                                   [(+ put-value delta-price) call-value]
                                                                   [put-value (- call-value delta-price)])]
-                           (recur next-put-value next-call-value)))))))))]
+                           (recur  next-put-value next-call-value)))))))))]
       (map f (reduce #(map + % (deref %2))
                      [0. 0.]
                      (doall (repeatedly (.. Runtime getRuntime availableProcessors)
